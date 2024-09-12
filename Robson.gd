9a4e2e546a284aef8fe4bf4bcade9d0e4f49ea63
Franciscco018 <francisco.anderson23@nave.org.br>
2024-09-12 19:17:05 -0300

    Amém
    a

--- Robson.gd
+++ Robson.gd
@@ -12,8 +12,10 @@ var vida = 3
 var knockF = -100
 var xp = 0
 var speed = 300
+var distancia = 1
 
 var last_dir = "down"
+
 
 func _physics_process(delta):
 	if velocity.y > 0:
@@ -35,9 +37,6 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, speed)
 		$AniRobson.play("idle_"+last_dir)
 	move_and_slide()
-	
-	if xp == 50:
-		xp = 0
 		
 	var mouse_pos = get_global_mouse_position()
 		
@@ -50,26 +49,28 @@ func _physics_process(delta):
 	var fps = Engine.get_frames_per_second()
 	
 	var dir = global_position.direction_to(mouse_pos)
-	var distancia = 1
 	if Input.is_action_pressed("attack"):
 		distancia = 30
 		$arma/AArma/ColArma.disabled = false
 	else:
 		$arma/AArma/ColArma.disabled = true
 	$arma.position = dir * distancia
+	distancia = 1
 	
-	if $"../CanvasLayer/Xp".value == 50:
+	if $"../CanvasLayer/Xp".value == 1:
 		$"../CanvasLayer/Xp".value = 0
+		$"../CanvasLayer/Cartas/ColorRect".visible = true
+		get_tree().paused = true
 		
 	if vida == 0:
 		get_tree().change_scene_to_file("res://game_over.tscn")
-	
+		
 	if Input.is_action_pressed("esc"):
 		$"../CanvasLayer/Control/ColorRect".visible = true
 		get_tree().paused = true
-		voltar.grab_focus()
-
 		
+	$TextureRect/Vida/valorVida.text = str(vida)
+	
 func _on_area_robson_area_entered(area):
 	if area.is_in_group("lixo"):
 		area.queue_free()
@@ -78,26 +79,23 @@ func _on_area_robson_area_entered(area):
 		$"../coleta".play()
 		
 	if area.is_in_group("enemy"):
-		area.queue_free()
-		$"../CanvasLayer/Xp".value +=1
 		vida -=1
-		print(vida)
-		$Vida.value -=1
+		$TextureRect/Vida.value -=1
 		$"../hit".play()
+		
 		knock = true
 		var dir_enemy = area.get_parent().target_pos * 2000
 		velocity = dir_enemy
 		await(get_tree().create_timer(0.1).timeout)
 		knock = false
-	
+		
 
 func _on_spawn_lixo_timeout():
 	pass # Replace with function body.
 
 func _on_a_arma_area_entered(area):
 	if area.is_in_group("enemy"):
-		if $arma/AArma/ColArma.disabled == true:
-			area.queue_free()
+		area.queue_free()
 		$"../CanvasLayer/Xp".value +=1
 
 
@@ -115,3 +113,22 @@ func _on_voltar_mouse_entered():
 	$"../menu".play()
 func _on_menu_mouse_entered():
 	$"../menu".play()
+
+
+func _on_btn_v_pressed() -> void:
+	$TextureRect/Vida.max_value += 2
+	vida += 2
+	get_tree().paused = false
+	$"../CanvasLayer/Cartas/ColorRect".visible = false
+
+
+func _on_btn_vel_pressed() -> void:
+	speed += 100
+	get_tree().paused = false
+	$"../CanvasLayer/Cartas/ColorRect".visible = false
+
+
+func _on_btn_dist_pressed() -> void:
+	distancia += 40
+	get_tree().paused = false
+	$"../CanvasLayer/Cartas/ColorRect".visible = false
--- cartas.gd
+++ cartas.gd
@@ -1,12 +1,12 @@
-extends Node2D
+extends Control
 
-@onready var NomeV:Label= $Control/Vida/NomeV
-@onready var NomeVel:Label = $Control/Vel/NomeVel
-@onready var NomeDist:Label = $Control/Dist/NomeDist
+@onready var NomeV:Label= $ColorRect/HBoxContainer/Vida/NomeV 
+@onready var NomeVel:Label = $ColorRect/HBoxContainer/Vel/NomeVel
+@onready var NomeDist:Label = $ColorRect/HBoxContainer/Dist/NomeDist
 
-@onready var DescricaoV:Label = $Control/Vida/DescricaoV
-@onready var DescricaoVel:Label = $Control/Vel/DescricaoVel
-@onready var DescricaoDist:Label = $Control/Dist/DescricaoDist
+@onready var DescricaoV:Label = $ColorRect/HBoxContainer/Vida/DescricaoV
+@onready var DescricaoVel:Label = $ColorRect/HBoxContainer/Vel/DescricaoVel
+@onready var DescricaoDist:Label = $ColorRect/HBoxContainer/Dist/DescricaoDist
 
 var cartas:Dictionary = {
 	"Acelerar" = {"Nome":"Acelerar", "Descrição": "Ganhe 100 de velocidade"},
@@ -14,7 +14,13 @@ var cartas:Dictionary = {
 	"Vida" = {"Nome":"Vida", "Descrição": "Ganhe 2 de vida"}
 }
 
-var list_cartas:Array = [cartas["Acelerar"],cartas["Range"],cartas["Vida"]]
+var list_cartas:Array = [cartas["Vida"], cartas["Acelerar"],cartas["Range"]]
 
 func _ready():
-	NomeV.text = list_cartas[1]
+	NomeV.text = list_cartas[0]["Nome"]
+	NomeVel.text = list_cartas[1]["Nome"]
+	NomeDist.text = list_cartas[2]["Nome"]
+	
+	DescricaoV.text = list_cartas[0]["Descrição"]
+	DescricaoVel.text = list_cartas[1]["Descrição"]
+	DescricaoDist.text = list_cartas[2]["Descrição"]
